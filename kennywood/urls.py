b2fd45886842e6e108ff3e586e3a3525da121688
Sam Pita <samanthakpita@gmail.com>
2020-02-17 15:24:45 -0600

    configured DefaultRouter for ParkAreas

--- kennywood/urls.py
+++ kennywood/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
+from kennywoodapi.views import ParkAreas
 from kennywoodapi.models import *
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'parkareas', ParkAreas, 'parkarea')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),   
 ]
